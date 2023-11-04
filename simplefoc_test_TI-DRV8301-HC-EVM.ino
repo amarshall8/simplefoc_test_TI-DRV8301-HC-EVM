d29fc966a5f0762cf01655b43ef0f7e1c130e0ae
Alec <91332112+amarshall8@users.noreply.github.com>
2023-11-03 23:45:01 -0500

    updated files

--- simplefoc_test_TI-DRV8301-HC-EVM.ino
+++ simplefoc_test_TI-DRV8301-HC-EVM.ino
@@ -1,15 +1,48 @@
 #include <SimpleFOC.h>
 
-#define pot_pin 4
+//nominal supply voltage
+#define suppVoltage 30
+#define maxVoltage 32
+
+#define pot_pin PE_10
+
+//motor parameters
+#define pole_pairs 20
+#define KV_rating 10.2
+
+//motor PWM outputs
+#define pwm_A PC_7
+#define pwm_B PB_5
+#define pwm_C PB_3
+
+//DRV8301 specific pins
+#define EN_GATE PF_13
+#define M_PWM PE_9 
+#define M_OC PE_11
+#define OC_ADJ PF_14
+
+//hall sensor pins
+#define hall_A PE_13
+#define hall_B PF_15
+#define hall_C PG_14
+
+//current sense pins
+#define DC_CAL PD_14
+#define IA_FB PA_5
+#define IB_FB PA_6
+#define IC_FB PA_7
+
+#define CurrGain 21.65
+#define R_shunt 0.002
 
 //  BLDCMotor( pole_pairs , ( phase_resistance, KV_rating  optional) )
-BLDCMotor motor = BLDCMotor(11, 9.75);
+BLDCMotor motor = BLDCMotor(pole_pairs, KV_rating);
 
 // Hall sensor instance
 // HallSensor(int hallA, int hallB , int hallC , int pp)
 //  - hallA, hallB, hallC    - HallSensor A, B and C pins
 //  - pp                     - pole pairs
-HallSensor sensor = HallSensor(2, 3, 4, 11);
+HallSensor sensor = HallSensor(hall_A, hall_B, hall_C, pole_pairs, EN_GATE);
 
 // Interrupt routine initialization
 // channel A and B callbacks
@@ -18,12 +51,12 @@ void doB(){sensor.handleB();}
 void doC(){sensor.handleC();}
 
 //  BLDCDriver3PWM( pin_pwmA, pin_pwmB, pin_pwmC, enable (optional))
-BLDCDriver3PWM driver = BLDCDriver3PWM(9, 5, 6, 8);
+BLDCDriver3PWM driver = BLDCDriver3PWM(pwm_A, pwm_B, pwm_C);
 
 // instantiate sensor 
 
 //  LowsideCurrentSense(shunt_resistance, gain, adc_a, adc_b, adc_c)
-LowsideCurrentSense current_sense = LowsideCurrentSense(0.01, 50, A0, A1, A2);
+LowsideCurrentSense current_sense = LowsideCurrentSense(R_shunt, CurrGain, IA_FB, IB_FB, IC_FB);
 
 //instantiate commander
 Commander commander = Commander(Serial);
@@ -43,9 +76,9 @@ void setup() {
   // pwm frequency to be used [Hz]
   driver.pwm_frequency = 20000;
   // power supply voltage [V]
-  driver.voltage_power_supply = 12;
+  driver.voltage_power_supply = suppVoltage;
   // Max DC voltage allowed - default voltage_power_supply
-  driver.voltage_limit = 12;
+  driver.voltage_limit = maxVoltage;
   // driver init
   driver.init();
 
@@ -93,7 +126,7 @@ void loop() {
 
   // velocity control loop function
   // setting the target velocity or 2rad/s
-  motor.move(2);
+  motor.move(pot_val);
 
   // monitoring function outputting motor variables to the serial terminal 
   motor.monitor();
